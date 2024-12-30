f6ab342a49fc32648d292d240c042ee89b50b859
sahil016 <sahildiwan8888@gmail.com>
2024-12-30 11:13:59 +0530

    nn

--- myproject/myapp1/models.py
+++ myproject/myapp1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(unique=True)
+    mobile=models.BigIntegerField(unique=True)
+    password=models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name
--- myproject/myapp1/views.py
+++ myproject/myapp1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,'home.html')
@@ -7,8 +8,48 @@ def home(request):
 def index(request):
     return render(request,'index.html')
 
-def blog(request):
-    return render(request,'blog-single.html')
 
 def about(request):
     return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+""" 
+def signup(request):
+    if request.methos=="POST":
+        
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        confirm_password=request.POST['cpassword']
+        if password==confirm_password:
+            user=user.objects.create_user(username,email,password)
+            user.save()
+            return render(request,'login.html')
+        else:
+            return render(request,'signup.html',{'error':'password and confirm password do not match'})
+    
+    return render(request, 'signup.html') """
+    
+def signup(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['cpassword']
+
+        if password == confirm_password:
+            if User.objects.filter(name=name).exists():
+                return render(request, 'signup.html', {'error': 'Username already exists'})
+            if User.objects.filter(email=email).exists():
+                return render(request, 'signup.html', {'error': 'Email already exists'})
+
+            user = User.objects.create_user(name=name, email=email, password=password)
+            user.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+            ##  # Redirect to the login page after successful signup
+        else:
+            return render(request, 'signup.html', {'error': 'Passwords do not match'})
+
+    return render(request, 'signup.html')
